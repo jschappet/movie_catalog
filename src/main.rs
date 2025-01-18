e06da56f612345f05bd693d1a71664ee53661410
Schappet <jschappet@gmail.com>
2025-01-17 17:12:45 -0800

    Adding Delete Meta Data

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::serde::{Deserialize, Serialize, json::Json};
+use regex::Regex;
+
+// use rocket::serde::{Deserialize, Serialize};
 use rocket::fs::{FileServer, NamedFile};
 use rocket::State;
 use std::path::Path;
-use std::sync::Mutex;
+//use std::sync::Mutex;
 use rocket_dyn_templates::{Template, context};
 //use sqlx::types::Json;
 use sqlx::query;
 
 use dotenv::dotenv;
 use std::env;
-use sqlx::{Encode, Row};
+use sqlx::Row;
+
+mod movies;
+use movies::MovieMetadata;
 
 
 mod db;
 use db::{init_db, DbPool};
 
-// Struct to represent a user
-#[derive(Debug, Serialize, Deserialize, Clone)] // Added Clone here
-#[serde(crate = "rocket::serde")]
-struct User {
-    id: u32,
-    name: String,
-    email: String,
-}
 
 // Application state
 #[derive(Clone)]
@@ -35,75 +32,13 @@ struct AppState {
     client: reqwest::Client,
 }
 
-/* // Handler to get a user by ID
-#[get("/<id>")]
-async fn get_user(id: u32, state: &State<AppState>) -> Option<Json<User>> {
-    let users = state.users.lock().unwrap();
-    if let Some(user) = users.iter().find(|user| user.id == id) {
-        println!("Page Title: {}", state.title);
-        return Some(Json(user.clone()));
-    }
-    None
-} */
-
-#[derive(Deserialize,Serialize)]
-struct MovieList {
-    titles: Vec<String>,
-}
-
-
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "PascalCase")]
-struct MovieMetadata {
-    title:  Option<String>,
-    year:  Option<String>,
-    rated:  Option<String>,
-    released:  Option<String>,
-    runtime:  Option<String>,
-    genre:  Option<String>,
-    director:  Option<String>,
-    writer:  Option<String>,
-    actors:  Option<String>,
-    plot:  Option<String>,
-    language:  Option<String>,
-    country:  Option<String>,
-    awards:  Option<String>,
-    poster:  Option<String>,
-    ratings: Option<Vec<Rating>>,
-    metascore:  Option<String>,
-    #[serde(rename = "imdbRating")]
-    imdb_rating:  Option<String>,
-    #[serde(rename = "imdbVotes")]
-    imdb_votes:  Option<String>,
-    #[serde(rename = "imdbID")]
-    imdb_id:  Option<String>,
-    #[serde(rename = "Type")]
-    movie_type:  Option<String>, // Reserved keyword, escaped
-    dvd: Option<String>,
-    #[serde(rename = "BoxOffice")]
-    box_office:  Option<String>,
-    production:  Option<String>,
-    website:  Option<String>,
-    response: String,
-    #[serde(rename = "Error")]
-    error: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Encode, sqlx::Type)]
-#[serde(rename_all = "PascalCase")]
-struct Rating {
-    source: String,
-    value: String,
-}
-
 
 #[get("/movies")]
-async fn movies(state: &State<AppState>) -> Template {
+async fn get_movies(state: &State<AppState>) -> Template {
 
     let db = &state.db_pool;
 
-    let rows = query("SELECT title, metadata FROM movies")
+    let rows = query("SELECT title, metadata FROM movies order by title")
         .fetch_all(db)
         .await
         .expect("Failed to fetch movies");
@@ -120,24 +55,16 @@ async fn movies(state: &State<AppState>) -> Template {
                 movies.push(md); 
                     // Add the structured object to the list
             }
-            Err(e) => {
-                eprintln!("Failed to parse metadata: {}", e);
+            Err(_) => {
+                //eprintln!("Failed to parse metadata: {}", e);
             }
         }
-        
+    
     }
 
     Template::render("movie_card", context! { movies })
 }
 
-/* // Handler to add a new user
-#[post("/", data = "<new_user>")]
-async fn add_user(new_user: Json<User>, state: &State<AppState>) -> &'static str {
-    let mut users = state.users.lock().unwrap();
-    users.push(new_user.into_inner());
-    println!("Page Title: {}", state.title);
-    "User added successfully!"
-} */
 
 // Fallback to serve the `index.html` file when no route matches
 #[get("/")]
@@ -145,62 +72,122 @@ async fn serve_index() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/index.html")).await.ok()
 }
 
+// Fallback to serve the `index.html` file when no route matches
+#[delete("/metadata")]
+async fn delete_metadata(state: &State<AppState>) ->  &'static str {
+    let db: &sqlx::Pool<sqlx::Sqlite> = &state.db_pool;
+
+    sqlx::query("UPDATE movies SET metadata=''")
+    .execute(db)
+    .await
+    .expect("Failed to delete metadata");
+    "All Movie Metadata Deleted"
+}
+
+fn escape_plot(plot: Option<String>) -> Option<String> {
+    plot.map(|p| p.replace("'", "&#39;"))
+}
+
+fn process_title_and_year(title_with_year: &str) -> (String, Option<String>) {
+    // Regular expression to capture the title and year in parentheses
+    let re = Regex::new(r"^(.*)\s\((\d{4})\)$").unwrap();
+
+    if let Some(captures) = re.captures(title_with_year) {
+        // Extract the title and year
+        let title = captures.get(1).map_or("", |m| m.as_str()).to_string();
+        let year = captures.get(2).map_or("", |m| m.as_str()).to_string();
+        (title, Some(year))
+    } else {
+        // If no match, return the original string and None for the year
+        (title_with_year.to_string(), None)
+    }
+}
+
+
 async fn fetch_metadata(state: &AppState, title: &str) -> Result<String, reqwest::Error> {
-    let url = format!(
+
+    let (title_without_year, year) = process_title_and_year(title);
+
+    let url = match year {
+        Some(y) => format!(
+            "http://www.omdbapi.com/?t={}&y={}&apikey={}",
+            title_without_year.replace("&", "%26"), y,  state.omdb_api_key
+        ),
+        None => format!(
+            "http://www.omdbapi.com/?t={}&apikey={}",
+            title_without_year.replace("&", "%26"),  state.omdb_api_key
+        ),
+    };
+
+    /* let url = format!(
         "http://www.omdbapi.com/?t={}&apikey={}",
-        title, state.omdb_api_key
-    );
+        title_without_year.replace("&", "%26"), state.omdb_api_key
+    ); */
 
     let res = state
         .client.get(&url)
         .send().await?
         .json::<serde_json::Value>().await?;
     Ok(res.to_string())
-    /* Ok(MovieMetadata {
-        title: res["Title"].as_str().unwrap_or_default().to_string(),
-        year: res["Year"].as_str().unwrap_or_default().to_string(),
-        genre: res["Genre"].as_str().unwrap_or_default().to_string(),
-        director: res["Director"].as_str().unwrap_or_default().to_string(),
-        plot: res["Plot"].as_str().unwrap_or_default().to_string(),
-    })
-    */
+    
 }
+
 // Handler for file uploads
 #[post("/upload", data = "<data>")]
 async fn upload_movies(state: &State<AppState>, data: String) -> &'static str {
-    let db = &state.db_pool;
+    let db: &sqlx::Pool<sqlx::Sqlite> = &state.db_pool;
     
-    for title in data.titles {
-         
-        let meta = fetch_metadata(&state, title.as_str()).await;
-        match meta {
-            Ok(meta) => {
-                println!("{:?}", meta);
+    // Parse JSON data
+    let parsed_data: serde_json::Value = match serde_json::from_str(&data) {
+        Ok(json) => json,
+        Err(_) => {
+            return "Invalid JSON format";
+        }
+    };
+    if let Some(titles) = parsed_data["titles"].as_array() {
 
-                let metadata: MovieMetadata = serde_json::from_str(&meta)
-                    .expect("ERROR");
-
-                let metadata_json = to_string(&meta).expect("Failed to serialize metadata");
-                println!("{}", metadata_json);
-                sqlx::query("INSERT INTO movies (title, metadata) VALUES (?, ?)")
-                    .bind(title.clone())
-                    .bind(json!(metadata))
-                    .execute(db)
-                    .await
-                    .expect("Failed to insert movie");
+        for title in titles {
             
-                println!("Done Update: {}", title);
-            }
-            Err(e) => {
-                sqlx::query("INSERT INTO movies (title) VALUES (?)")
+            let meta = fetch_metadata(&state, 
+                    title.as_str().unwrap_or_default()).await;
+            match meta {
+                Ok(meta) => {
+                    println!("{:?}", meta);
+
+                    let mut metadata: MovieMetadata = serde_json::from_str(&meta)
+                        .expect("ERROR");
+
+                    let metadata_json = serde_json::to_string(&meta).expect("Failed to serialize metadata");
+                    println!("{}", metadata_json);
+                    if metadata.response == "False" {
+                        metadata.title = Some(String::from(title.as_str().unwrap_or(""))); 
+                    } 
+                    metadata.plot = escape_plot(metadata.plot);
+                    metadata.title = escape_plot(metadata.title);
+                    
+
+                    sqlx::query("INSERT INTO movies (title, metadata) VALUES (?, ?)")
                     .bind(title.clone())
+                    .bind(serde_json::json!(metadata))
                     .execute(db)
                     .await
                     .expect("Failed to insert movie");
-                eprintln!("Failed to fetch metadata for {}: {:?}", title, e);
-                () // Wrap it in a vector to return it as JSON
-            }
-        }   
+
+                    
+                
+                    println!("Done Update: {}", title);
+                }
+                Err(e) => {
+                    sqlx::query("INSERT INTO movies (title) VALUES (?)")
+                        .bind(title.clone())
+                        .execute(db)
+                        .await
+                        .expect("Failed to insert movie");
+                    eprintln!("Failed to fetch metadata for {}: {:?}", title, e);
+                    () // Wrap it in a vector to return it as JSON
+                }
+            }   
+        }
     }
     "Movies uploaded successfully!"
 }
@@ -224,7 +211,7 @@ async fn rocket() -> _ {
 
     rocket::build()
         .manage(app_state) // Add shared state
-        .mount("/", routes![movies])
+        .mount("/", routes![get_movies, upload_movies, delete_metadata])
         .mount("/foo", routes![serve_index])
         .mount("/", FileServer::from("static")) // Serve static files
         .attach(Template::fairing())
---
+++ src/movies.rs
@@ -0,0 +1,53 @@
+use rocket::serde::{Deserialize, Serialize};
+use sqlx::Encode;
+
+#[derive(Deserialize,Serialize)]
+pub struct MovieList {
+    titles: Vec<String>,
+}
+
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct MovieMetadata {
+    pub title:  Option<String>,
+    year:  Option<String>,
+    rated:  Option<String>,
+    released:  Option<String>,
+    runtime:  Option<String>,
+    genre:  Option<String>,
+    director:  Option<String>,
+    writer:  Option<String>,
+    actors:  Option<String>,
+    pub plot:  Option<String>,
+    language:  Option<String>,
+    country:  Option<String>,
+    awards:  Option<String>,
+    poster:  Option<String>,
+    ratings: Option<Vec<Rating>>,
+    metascore:  Option<String>,
+    #[serde(rename = "imdbRating")]
+    imdb_rating:  Option<String>,
+    #[serde(rename = "imdbVotes")]
+    imdb_votes:  Option<String>,
+    #[serde(rename = "imdbID")]
+    imdb_id:  Option<String>,
+    #[serde(rename = "Type")]
+    movie_type:  Option<String>, // Reserved keyword, escaped
+    dvd: Option<String>,
+    #[serde(rename = "BoxOffice")]
+    box_office:  Option<String>,
+    production:  Option<String>,
+    website:  Option<String>,
+    pub response: String,
+    #[serde(rename = "Error")]
+    error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Encode, sqlx::Type)]
+#[serde(rename_all = "PascalCase")]
+struct Rating {
+    source: String,
+    value: String,
+}
